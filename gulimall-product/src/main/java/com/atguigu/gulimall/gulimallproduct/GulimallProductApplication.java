a2e3415253750921ef13c9a128ad89c31a0a7e06
thiszxw <3327332153@qq.com>
2024-05-06 11:03:00 +0800

    项目初始化构建

--- gulimall-product/src/main/java/com/atguigu/gulimall/gulimallproduct/GulimallProductApplication.java
+++ gulimall-product/src/main/java/com/atguigu/gulimall/gulimallproduct/GulimallProductApplication.java
@@ -1,9 +1,22 @@
 package com.atguigu.gulimall.gulimallproduct;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
+/**
+ * 整合mybatis-plus
+ * 1.引入依赖
+ * 2.配置数据源, 配置mybatis-plus
+ * 3.@MapperScan用于扫描指定包下的类，以便于Spring Boot应用能够自动发现并加载这些类并将它们注册为Spring的Bean
+ * 4.告诉mybatis-plus,sql的映射文件在哪，在yml中书写
+ */
+@MapperScan("com.atguigu.gulimall.product.dao")
 @SpringBootApplication
+@ComponentScan(basePackages = "com.atguigu.gulimall")//指定扫描的包路径。
+
+
 public class GulimallProductApplication {
 
     public static void main(String[] args) {
--- gulimall-product/src/test/java/com/atguigu/gulimall/gulimallproduct/GulimallProductApplicationTests.java
+++ gulimall-product/src/test/java/com/atguigu/gulimall/gulimallproduct/GulimallProductApplicationTests.java
@@ -1,13 +1,37 @@
 package com.atguigu.gulimall.gulimallproduct;
 
+import com.atguigu.gulimall.product.entity.BrandEntity;
+import com.atguigu.gulimall.product.service.BrandService;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.annotation.Resource;
+import java.util.List;
 
 @SpringBootTest
 class GulimallProductApplicationTests {
 
+
+    @Resource
+    BrandService brandService;
     @Test
-    void contextLoads() {
+    public void contextLoads() {
+//        BrandEntity brandEntity = new BrandEntity();
+//        brandEntity.setName("zxw");
+//        brandEntity.setBrandId(1L);
+//        brandEntity.setDescript("zxw");
+        //添加操作
+//        brandService.save(brandEntity);
+//        System.out.println("保存成功到。。。");
+
+        //修改操作
+//        brandService.updateById(brandEntity);
+
+        //查询操作
+        List<BrandEntity> list = brandService.list(new QueryWrapper<BrandEntity>().eq("brand_id", 1L));
+        list.forEach(System.out::println);
+
     }
 
 }
